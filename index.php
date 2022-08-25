cad3063265b746bc96ea8108086c6abeb1d3286c
Laura Brooks <laura.brooks0@outlook.com>
2022-08-25 14:53:25 +0900

    image resize

--- index.php
+++ index.php
@@ -9,7 +9,7 @@ use Zara4\API\ImageProcessing\ProcessedImage;
 
 
 $apiClient = new Client(getenv("ZARA_4_API_CLIENT_ID"), getenv("ZARA_4_API_CLIENT_SECRET"));
-$originalImage = new LocalImageRequest("images/bio pic.jpg", "images/alt_speaker.jpg", "images/bw_bio_pic.jpg", "images/jazz.jpg", "images/jazz2.jpg", "images/jazz3.jpg", "images/wide.jpg");
+$originalImage = new LocalImageRequest("images/bio pic.jpg", "images/alt_speaker.png", "images/bw_bio_pic.jpg", "images/jazz.jpg", "images/jazz2.jpg", "images/jazz3.jpg", "images/wide.jpg");
 $processedImage = $apiClient->processImage($originalImage);
 $apiClient->downloadProcessedImage($processedImage, "images/output.jpg");
 
